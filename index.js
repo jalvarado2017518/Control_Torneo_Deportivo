64e11adb330e1375345e52d31d623397fd6bdda8
Julio Eduardo Gustavo Alvarado Ramirez <88795773+jalvarado2017518@users.noreply.github.com>
2022-06-11 16:15:19 -0600

    actualizando index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const usuarioControlador = require('./src/controllers/usuarios.controller');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/ControlTorneoDeportivo', { useNewUrlParser: true, useUnifiedTopology: true}).then(() =>{
     console.log("Se encuentra conectado a la base de datos");
-    app.listen(process.env.PORT || 3000, function() {
+    app.listen(3000, function() {
         console.log("hola mundo, estamos en el puerto 3000!")
     })
 }).catch(error => console.log(error))
